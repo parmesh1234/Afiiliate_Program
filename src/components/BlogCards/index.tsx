9da852413a170d4fc59cc50851ec69fa9a7d9fce
thatzmypet <thatzmypet@gmail.com>
2024-05-20 23:13:02 +0530

    Added more products

--- src/components/BlogCards/index.tsx
+++ src/components/BlogCards/index.tsx
@@ -5,6 +5,9 @@ import diaperImage from "../../assets/product_images/daipers.jpg";
 import shampooImage from "../../assets/product_images/shampoo.jpg";
 import TrimmerImage from "../../assets/product_images/trimmer.jpg";
 import Ariel from "../../assets/product_images/ariel.jpg";
+import Dumbell from "../../assets/product_images/dumbell.jpg";
+import LymioTshirt from "../../assets/product_images/lymiotshirt.jpg";
+
 import { useMedia } from "react-use";
 
 type indexProps = {};
@@ -49,7 +52,20 @@ const Blogcards: React.FC<indexProps> = () => {
       buttonLink:
         "https://amzn.to/3wICRvk",
     },
-
+    {
+      image: Dumbell,
+      title: "Sports",
+      heading: "Lifelong PVC Hex Dumbbells Pack of 2 for Home Gym",
+      description: "Set of 2 PVC hex dumbbells for resistance training; each dumbbell weighs 2 kg *2 total 4 kg weight Made in India",
+      buttonLink: "https://amzn.to/3ypGqY3"
+    },
+    {
+      image: LymioTshirt,
+      title: "Clothing",
+      heading: "Lymio Men T-Shirt || T-Shirt for Men || Plain T Shirt || T-Shirt (Polo-18-21)",
+      description: "Men T Shirt, Color Disclaimer: Product color might slightly vary due to photographic lighting sources or your monitor settings",
+      buttonLink: "https://amzn.to/4br6LDp"
+    }
 
   ];
   return (
